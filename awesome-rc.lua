81ce239e094cc7379fe75878ec07a4cfa4bf6d21
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2015-03-16 17:39:51 +0100

    Awesome has default nmaster of 0.

--- awesome-rc.lua
+++ awesome-rc.lua
@@ -62,6 +62,9 @@ tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
     tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, awful.layout.suit.tile.bottom)
+    for name,tag in pairs(tags[s]) do
+        awful.tag.setnmaster(0, tag)
+    end
 end
 -- }}}
 
