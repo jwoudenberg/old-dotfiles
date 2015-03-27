c5d6159b01b7cf9af6f8022df7f0bbd07011e7d3
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2015-03-27 20:19:37 +0100

    Updated awesome rc.lua.
    Removed scroll wheel bindings that had me switching tags by accident.

--- awesome-rc.lua
+++ awesome-rc.lua
@@ -83,11 +83,7 @@ mytaglist = {}
 
 mytaglist.buttons = awful.util.table.join(
                     awful.button({ }, 1, awful.tag.viewonly),
-                    awful.button({ modkey }, 1, awful.client.movetotag),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ modkey }, 3, awful.client.toggletag),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
+                    awful.button({ }, 3, awful.tag.viewtoggle)
                     )
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
@@ -114,14 +110,6 @@ mytasklist.buttons = awful.util.table.join(
                                               else
                                                   instance = awful.menu.clients({ width=250 })
                                               end
-                                          end),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                              if client.focus then client.focus:raise() end
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                              if client.focus then client.focus:raise() end
                                           end))
 
 for s = 1, screen.count() do
