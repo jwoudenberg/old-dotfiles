7baf42b56115399f47768821dc950ac353e79a1d
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-05-03 22:52:41 +0200

    Improve collapse script

--- config/fish/functions/collapse-lines.fish
+++ config/fish/functions/collapse-lines.fish
@@ -1,12 +1,22 @@
+# Take a markdown file where sentences start on individual lines,
+# and return the text with paragraphs collapsed onto a single line.
 function collapse-lines
-    set text ""
+    set in_frontmatter false
     while read -l line
-        if [ $line = "" ]
-            echo -en $text
-            set text "\n\n"
-        else
-            set text "$text$line "
+        switch $line
+            case '---'
+                set in_frontmatter not eval $in_frontmatter
+                echo $line
+            case ''
+                echo -e "\n"
+            case '#*'
+                echo $line
+            case '*'
+                if eval $in_frontmatter
+                    echo $line
+                else
+                    echo -n "$line "
+                end
         end
     end
-    echo -en $text
 end
