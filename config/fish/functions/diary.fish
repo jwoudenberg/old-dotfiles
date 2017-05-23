56ec405869bae5f4b09ffbed01c3c21c39b1b40c
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-05-23 08:50:18 +0200

    Diary script removes empty entries

--- config/fish/functions/diary.fish
+++ config/fish/functions/diary.fish
@@ -2,8 +2,14 @@ function diary
     set date (date "+%y-%m-%d")
     mkdir -p ~/docs/diary
     set entry ~/docs/diary/$date.md
+    set title "# $date"
     if not test -e $entry
-        echo "# $date" >$entry
+        echo "$title" > $entry
     end
     eval $EDITOR $entry
+    set content (string trim (cat $entry))
+    if test "$content" = "$title"
+        echo "Deleting empty diary entry."
+        rm $entry
+    end
 end
