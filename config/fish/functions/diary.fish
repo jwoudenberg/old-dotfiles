507ad92a7ceaeef58492a4b31c9ee84b68e57803
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-05-23 09:07:17 +0200

    Diary takes optional subtitle description

--- config/fish/functions/diary.fish
+++ config/fish/functions/diary.fish
@@ -3,10 +3,22 @@ function diary
     mkdir -p ~/docs/diary
     set entry ~/docs/diary/$date.md
     set title "# $date"
+
+    # Create the diary file if it doesn't exist.
     if not test -e $entry
         echo "$title" > $entry
     end
+
+    # Add a subtitle section if extra arguments are passed.
+    set subtitle_text (string join " " $argv)
+    if test -n "$subtitle_text"
+        echo -e "\n## $subtitle_text" >> $entry
+    end
+
+    # Allow the user to edit the entry.
     eval $EDITOR $entry
+
+    # Remove the entry if no text was added to it.
     set content (string trim (cat $entry))
     if test "$content" = "$title"
         echo "Deleting empty diary entry."
